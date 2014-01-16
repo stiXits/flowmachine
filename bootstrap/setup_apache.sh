d55c30408ae9ed74dabfad597951710c1f8701e1
Stix <stix@phcn.de>
2014-01-17 00:17:39 +0100

    fixed installscript environment

--- bootstrap/setup_apache.sh
+++ bootstrap/setup_apache.sh
@@ -1,5 +1,7 @@
+cd /vagrant/bootstrap
+
 echo "### setting httpd.conf ###"
-sudo cp bootstrap/config/httpd.conf /etc/httpd/conf/
+sudo cp config/httpd.conf /etc/httpd/conf/
 
 echo "### starting httpd daemon"
 sudo systemctl enable httpd
--- bootstrap/setup_composer.sh
+++ bootstrap/setup_composer.sh
@@ -1,6 +1,7 @@
 
 #install composer
 echo "### retrieving composer package from AUR ###"
+cd /vagrant
 wget https://aur.archlinux.org/packages/ph/php-composer/php-composer.tar.gz
 tar -xf php-composer.tar.gz
 rm php-composer.tar.gz
