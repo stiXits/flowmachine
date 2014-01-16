108262272d864f6f72ef9823a7ffb2f8606e451d
Stix <stix@phcn.de>
2014-01-16 18:55:53 +0100

    cleaned up bootstrap script and gitignore

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-sudo pacman -Syu
-sudo pacman -S base-devel apache php php-apache mariadb
-
-#install composer
-wget https://aur.archlinux.org/packages/ph/php-composer/php-composer.tar.gz
-tar -xf php-composer.tar.gz
-rm php-composer.tar.gz
-cd php-composer
-makepkg
