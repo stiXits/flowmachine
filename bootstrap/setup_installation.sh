506b7ff4f6fab70b78c5aab2496733fcfad511a7
Stix <stix@phcn.de>
2014-01-17 01:23:42 +0100

    added expect to standard packages

--- bootstrap/setup_installation.sh
+++ bootstrap/setup_installation.sh
@@ -2,4 +2,4 @@ echo "### performing full system upgrade ###"
 sudo pacman -Syu
 
 echo "### installing startup packages ###"
-sudo pacman -S base-devel apache php php-apache mariadb
+sudo pacman -S base-devel apache php php-apache mariadb expect
