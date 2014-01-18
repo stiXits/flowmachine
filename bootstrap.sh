0ded2e116f27fbc3e1bcea742d67b22579946982
stix-desktop <stix@phcn.de>
2014-01-18 18:33:33 +0100

    changed to quiet arch update in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,4 @@
 # Configure .bash_profile
 echo "cd /vagrant" > ~vagrant/.bash_profile
+
+./bootstrap/setup_all.sh
--- bootstrap/setup_installation.sh
+++ bootstrap/setup_installation.sh
@@ -1,5 +1,5 @@
 echo "### performing full system upgrade ###"
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 echo "### installing startup packages ###"
-sudo pacman -S base-devel apache php php-apache mariadb expect
+sudo pacman -S base-devel apache php php-apache mariadb expect --noconfirm
