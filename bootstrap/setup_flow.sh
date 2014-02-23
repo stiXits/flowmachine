e64b9c16c0354fb83472c601d167a5d756ff207b
stix-desktop <stix@phcn.de>
2014-02-23 21:34:10 +0100

    added phpunit to composer.json

--- bootstrap/setup_flow.sh
+++ bootstrap/setup_flow.sh
@@ -5,7 +5,7 @@ sudo chmod 775 http
 
 cd http
 
-sudo -u vagrant composer create-project --dev --keep-vcs typo3/flow-base-distribution flow 2.0.0
+sudo -u vagrant composer create-project --dev --keep-vcs typo3/flow-base-distribution flow 2.0.0 phpunit/phpunit 3.7.* mikey179/vfsstream 1.1.*
 
 echo "### running composer ###"
 cd flow
