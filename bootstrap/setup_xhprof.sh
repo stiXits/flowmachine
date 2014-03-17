67b721bb4fa15f1a017ffd3fc206049028372812
stix-desktop <stix@phcn.de>
2014-03-17 22:17:35 +0100

    fixed xhprof rights problems

--- bootstrap/setup_xhprof.sh
+++ bootstrap/setup_xhprof.sh
@@ -1,20 +1,19 @@
-cd /vagrant
 echo "### building xhprof ###"
 
 echo "### retrieving xhprof package from AUR ###"
-sudo -u vagrant  wget https://aur.archlinux.org/packages/ph/php-xhprof/php-xhprof.tar.gz
-sudo -u vagrant tar -xf php-xhprof.tar.gz
-sudo -u vagrant rm php-xhprof.tar.gz
+wget https://aur.archlinux.org/packages/ph/php-xhprof/php-xhprof.tar.gz
+tar -xf php-xhprof.tar.gz
+rm php-xhprof.tar.gz
 
 echo "### making package ###"
 cd php-xhprof
-sudo -u vagrant makepkg
+makepkg
 
 echo "### installing xhprof package ###"
 sudo pacman -U `ls | grep php-xhprof` --noconfirm
 
 echo "### cleaning up ###"
 cd ..
-sudo -u vagrant rm -rf php-xhprof
+rm -rf php-xhprof
 
 cd /vagrant
